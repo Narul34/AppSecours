fb8afcfd537e902e1e37dfdf5dd4f140a6279b01
RockDafox <alpha8002@yahoo.fr>
2016-10-25 14:26:36 +0200

    Widget design OK Non fonctionnel

--- app/src/main/java/com/example/a34011_73_08/appsecours/SecoursWidget.java
+++ app/src/main/java/com/example/a34011_73_08/appsecours/SecoursWidget.java
@@ -9,60 +9,41 @@ import android.widget.Button;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import java.util.Random;
+
 /**
  * Implementation of App Widget functionality.
  */
 public class SecoursWidget extends AppWidgetProvider {
     public static final String OUVERTURE = "ouvrir";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-
-        CharSequence widgetText = "AppSecours";
-        // Prepare widget views
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.secours_widget);
-        views.setTextViewText(R.id.button, "Clic");
-
-        int rand = (int) Math.random()*1000;
-        views.setTextViewText(R.id.textView, Integer.toString(rand));
-
-        // Prepare intent to launch on widget click
-        Intent intent = new Intent(context, SecoursWidget.class);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-        intent.setAction(OUVERTURE);
-
-        // Launch intent on widget click
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-        views.setOnClickPendingIntent(R.id.button, pendingIntent);
-
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+        final int count = appWidgetIds.length;
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.secours_widget);
+        for (int i = 0; i < count; i++) {
+            int widgetId = appWidgetIds[i];
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.secours_widget);
 
 
-        for (int appWidgetId : appWidgetIds) {
 
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            // Prepare intent to launch on widget click
+            Intent intent = new Intent(context, SecoursWidget.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            intent.setAction(appWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+            // Launch intent on widget click
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.button112, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetIds, views);
+
         }
     }
-    @Override
-    public void onReceive(Context context, Intent intent){
-        super.onReceive(context, intent);
 
-        if(OUVERTURE.equals(intent.getAction())){
-            showNotification(context, "J'ai cliqué");
-        }
-
-
-    }
 
     @Override
     public void onEnabled(Context context) {
